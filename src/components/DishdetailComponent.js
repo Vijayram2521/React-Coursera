be34e6904a76bed20d1ae6b87ec5fb5dd0325a14
Vijay <vijayreddyjulakanti@gmail.com>
2020-07-30 12:31:38 +0530

    Functional COmponents

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,67 +1,64 @@
 import React, { Component } from 'react'; 
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props) ;
-
-        this.state= {
-            dish : null
-        }
+function RenderDish ({dish}) {
+    if (dish != null) {
+        return (
+                 <React.Fragment>
+              <div className="container">
+             <div className="row">
+               <div className="col-12 col-md-5 m-1">
+                 <Card>
+                  <CardImg width="100%" top src={dish.image} alt={dish.name} />
+                   <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                   </CardBody>
+                 </Card>
+               </div>
+               <div className="col-12 col-md-5 m-1">
+                      <RenderComments dishComments={dish.comments} />
+               </div>
+             </div>
+         </div>
+            </React.Fragment>  
+        ) ;
     }
-
-    renderComments(dishComments) {
-       if(dishComments != null) {
-           const comt = dishComments.map((com)=>{
-               return (
-                   <React.Fragment>
-                       <li>{com.comment}</li><br/>
-                       <li>-- {com.author}, {new Intl.DateTimeFormat('en-US',{year : 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(com.date)))}</li> <br/>
-                   </React.Fragment>
-               ) ;
-           }) ;
-         return(
-             <React.Fragment>
-               <ul className="list-unstyled">
-                   <h4>Comments</h4>                   
-                    {comt}
-               </ul>   
-             
-             </React.Fragment>
-         ) ; 
-       }
-    }
-    
-
-   render() {
-       const dish = this.props.dish ;
-       if (dish != null) {
-           return (
-               <div className="container">
-                    <React.Fragment>
-                 <div className="container">
-                <div className="row">
-                  <div className="col-12 col-md-5 m-1">
-                    <Card>
-                     <CardImg width="100%" top src={dish.image} alt={dish.name} />
-                      <CardBody>
-                       <CardTitle>{dish.name}</CardTitle>
-                       <CardText>{dish.description}</CardText>
-                      </CardBody>
-                    </Card>
-                  </div>
-                  <div className="col-12 col-md-5 m-1">
-                         {this.renderComments(dish.comments)}
-                  </div>
-                </div>
-            </div>
-               </React.Fragment>  
-               </div> 
-           ) ;
-       }
-       else
-          return(<div></div>)
-   }
+    else
+       return(<div></div>) ;
 }
+
+function RenderComments({dishComments}) {
+    if(dishComments != null) {
+        const comt = dishComments.map((com)=>{
+            return (
+                <React.Fragment>
+                    <li>{com.comment}</li><br/>
+                    <li>-- {com.author}, {new Intl.DateTimeFormat('en-US',{year : 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(com.date)))}</li> <br/>
+                </React.Fragment>
+            ) ;
+        }) ;   
+     return (
+         <React.Fragment>
+                      <h4>Comments</h4>
+            <ul className="list-unstyled">
+             {comt}
+         </ul>
+         </React.Fragment>
+     )   
+  }
+}
+
+const DishDetail = (props) => {
+    return(
+        <div className="container">
+            <div className="row">
+            <RenderDish dish={props.dish}/>            
+            </div>
+        </div>
+
+    ) ;
+}
+
 
 export default DishDetail ;
